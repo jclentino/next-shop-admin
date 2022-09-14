aa5434856fda1c4e32e2b5845370138b13dea1ff
jclentino <nuncaparesdecrear@gmail.com>
2022-09-14 11:05:11 -0500

    Autenticándonos en la API usando Axios

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,15 +1,24 @@
 import { useRef } from 'react';
 import { LockClosedIcon } from '@heroicons/react/24/solid';
+import { useAuth } from '@hooks/useAuth'; 
 
 export default function LoginPage() {
   const emailRef = useRef(null) 
   const passwordRef = useRef(null) 
+  const auth = useAuth(); 
 
   const handleSubmit = (e)=> { 
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    console.log(email,password);
+    
+    auth.signIn(email,password) 
+      .then(res => {
+        console.log('login success');
+      })
+      .catch(err => {
+        console.log('login error');
+      })
   } 
 
   return (
--- src/hook/useAuth.js
+++ src/hook/useAuth.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext, createContext } from "react";
 import Cookie from 'js-cookie'
 import axios from 'axios'
+import endPoints from "@services/api"; 
+
 
 const AuthContext = createContext();
 
@@ -16,9 +18,17 @@ export const useAuth = ()=> {
 function useProviderAuth(){
     const [user,setUser] = useState(null);
 
-    const signIn = (email,password)=> {
-        setUser('LoginExample');
-    }
+    const options = { 
+        Headers: {
+            accept: '*/*',
+            'Content-Type': 'application/json',
+        }
+    }; 
+
+    const signIn = async (email,password)=> { 
+        const { data: access_token } = await axios.post(endPoints.auth.login, { email, password}, options) 
+        console.log(access_token);
+    }; 
 
     return {
         user,
